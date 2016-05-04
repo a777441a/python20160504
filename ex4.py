6ddf0a585ac8cfaa553a75d9905f26f00b970878
a777441a <a777441a@yahoo.com.tw>
2016-05-04 11:07:21 +0800

    yoyo

---
+++ ex4.py
@@ -0,0 +1,18 @@
+scores = [60, 73, 81, 95, 34]
+n = 5
+i = 0
+total = 0
+var = 0
+while i < 5:
+    total += scores[i]
+    i += 1
+
+avg = total / n
+
+while i > 0:
+    i -= 1
+    var += (avg-scores[i])**2
+    
+print('total ' + str(total))
+print('average ' + str(avg))
+print('var '+str(var))
